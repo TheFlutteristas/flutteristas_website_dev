e5c4dcdc19c5749234daf8d1f071572830b20b83
Stef-GMS <stephanie@GeekMeSpeak.com>
2023-09-04 11:58:36 -0700

    Initial commit

---
+++ lib/app/app.dart
@@ -0,0 +1,45 @@
+import 'package:flutteristas/layout/page_frame.dart';
+import 'package:flutteristas/pages/column2_page.dart';
+import 'package:flutteristas/pages/column3_page.dart';
+import 'package:flutteristas/pages/contact_us_page.dart';
+import 'package:flutteristas/pages/follow_us_page.dart';
+import 'package:flutteristas/pages/not_found_page.dart';
+import 'package:flutteristas/pages/privacy_page.dart';
+import 'package:flutteristas/pages/two_sections.dart';
+import 'package:flutteristas/pages/welcome_page.dart';
+import 'package:flutteristas/pages/product_page_1.dart';
+import 'package:flutteristas/pages/product_page_note.dart';
+import 'package:flutteristas/pages/product_page.dart';
+import 'package:jaspr/components.dart';
+import 'package:jaspr/html.dart';
+import 'package:jaspr_router/jaspr_router.dart';
+
+final routes = [
+  WelcomePage.route,
+  TwoSectionPage.route,
+  Column2Page.route,
+  Column3Page.route,
+  ProductSummaryPage.route,
+  ProductPageOne.route,
+  ProductPageWithNote.route,
+  FollowUsPage.route,
+  ContactUsPage.route,
+  PrivacyPolicyPage.route,
+];
+
+class App extends StatelessComponent {
+  @override
+  Iterable<Component> build(BuildContext context) sync* {
+    yield Router(
+      routes: [
+        ShellRoute(
+          routes: routes,
+          builder: (BuildContext context, RouteState state, Component child) {
+            return PageFrame(child: child);
+          },
+        ),
+      ],
+      errorBuilder: NotFoundPage.builder,
+    );
+  }
+}
---
+++ lib/pages/contact_us_page.dart
@@ -0,0 +1,56 @@
+import 'package:jaspr/html.dart';
+import 'package:jaspr_router/jaspr_router.dart';
+
+class ContactUsPage extends StatelessComponent {
+  const ContactUsPage._();
+
+  static final route = Route(
+    path: '/contact-us',
+    title: 'Contact Us',
+    builder: (context, state) => ContactUsPage._(),
+  );
+
+  @override
+  Iterable<Component> build(BuildContext context) sync* {
+    yield p([
+      text(''),
+      a(
+        href: 'https://fluttercommunity.social/@Flutteristas',
+        target: Target.blank,
+        [text('Flutter Community Mastodon')],
+      ),
+    ]);
+    yield p([
+      text(''),
+      a(
+        href: 'https://github.com/TheFlutteristas',
+        target: Target.blank,
+        [text('GitHub')],
+      ),
+    ]);
+    yield p([
+      text(''),
+      a(
+        href: 'https://www.instagram.com/flutteristas/',
+        target: Target.blank,
+        [text('Instagram')],
+      ),
+    ]);
+    yield p([
+      text(''),
+      a(
+        href: 'https://www.linkedin.com/company/flutteristas',
+        target: Target.blank,
+        [text('LinkedIn')],
+      ),
+    ]);
+    yield p([
+      text(''),
+      a(
+        href: 'https://twitter.com/Flutteristas',
+        target: Target.blank,
+        [text('Twitter/X')],
+      ),
+    ]);
+  }
+}
