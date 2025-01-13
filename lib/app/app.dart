e771b379f8cdef3f2989a53b10b39d73e7d50cd1
Farah Khatib <eng.farahkh@gmail.com>
2025-01-13 20:17:09 +0400

    minor Jasper code restructuring

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -5,6 +5,7 @@ import 'package:flutteristas/pages/code_of_conduct.dart';
 // import 'package:flutteristas/pages/column3_page.dart';
 //import 'package:flutteristas/pages/contact_us_page.dart';
 import 'package:flutteristas/pages/flutteristas_conference.dart';
+import 'package:flutteristas/pages/flutteristas_map.dart';
 // import 'package:flutteristas/pages/follow_us_page.dart';
 import 'package:flutteristas/pages/not_found_page.dart';
 import 'package:flutteristas/pages/privacy_page.dart';
@@ -12,7 +13,7 @@ import 'package:flutteristas/pages/privacy_page.dart';
 import 'package:flutteristas/pages/welcome_page.dart';
 // import 'package:flutteristas/pages/product_page_1.dart';
 // import 'package:flutteristas/pages/product_page_note.dart';
-import 'package:flutteristas/pages/product_page.dart';
+//import 'package:flutteristas/pages/product_page.dart';
 import 'package:jaspr/components.dart';
 import 'package:jaspr/html.dart';
 import 'package:jaspr_router/jaspr_router.dart';
@@ -29,6 +30,7 @@ final routes = [
   // ProductPageWithNote.route,
   // FollowUsPage.route,
   // ContactUsPage.route,
+  FlutteristasMap.route,
   CodeOfConduct.route,
   PrivacyPolicyPage.route,
 ];
